c740edb73ae26296c340b02d72271873339eeacc
Michael Balda <michael.balda@gmail.com>
2017-10-19 16:28:41 +0200

    Added method "pipeline" that combines several operations.

--- GradientSample.py
+++ GradientSample.py
@@ -50,17 +50,68 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
     # 6) Return this mask as your binary_output image
     return binary_output
 
+def hls_select_s(img, thresh_s=(0, 255)):
+    # 1) Convert to HLS color space
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    S = hls[:,:,2]
+    # 2) Apply a threshold to the S channel
+    binary_output = np.zeros_like(S)
+    binary_output[(S > thresh_s[0]) & (S <= thresh_s[1])] = 1
+    # 3) Return a binary image of threshold result
+    return binary_output
+
+
+def pipeline(img, s_thresh=(0.3, 1.0), sm_thresh=(50, 130), sd_thresh=(0.7, 1.15)):
+    img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
+    l_channel = hls[:, :, 1]
+    s_channel = hls[:, :, 2]
+    # Sobel
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0, ksize=3)
+    sobely = cv2.Sobel(l_channel, cv2.CV_64F, 0, 1, ksize=3)
+
+    sobel_mag = np.sqrt(np.square(sobelx) + np.square(sobely))
+    scaled_sobel = np.uint8(255 * sobel_mag / np.max(sobel_mag))
+
+    # Threshold gradient magnitude
+    sm_binary = np.zeros_like(scaled_sobel)
+    sm_binary[(scaled_sobel >= sm_thresh[0]) & (scaled_sobel <= sm_thresh[1])] = 1
+
+    # Sobel again for gradient direction computation
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0, ksize=7)
+    sobely = cv2.Sobel(l_channel, cv2.CV_64F, 0, 1, ksize=7)
+    abs_sobelx = np.abs(sobelx)
+    abs_sobely = np.abs(sobely)
+    grad_dir = np.arctan2(abs_sobely, abs_sobelx)
+
+    # Create a binary mask where direction thresholds are met
+    sd_binary = np.zeros_like(grad_dir)
+    sd_binary[(grad_dir >= sd_thresh[0]) & (grad_dir <= sd_thresh[1])] = 1
+    #remove some isolated noise pixels
+    sd_binary = cv2.morphologyEx(sd_binary, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT,(3,3)))
+    # Threshold saturation channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel > s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    # Stack channels
+    color_binary = np.dstack((sd_binary, sm_binary, s_binary))
+
+    return color_binary
+
 # Run the function
-dir_binary = dir_threshold(image, sobel_kernel=15, thresh=(0.7, 1.3))
-mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
-combined = np.zeros_like(dir_binary)
-combined[((mag_binary == 1) & (dir_binary == 1))] = 1
+combined = pipeline(image, s_thresh=(0.3, 1.0), sm_thresh=(50, 130), sd_thresh=(0.7, 1.15))
+#combined = pipeline(image, (0, 255), (0, 255), (0.7, np.pi / 2))
+#dir_binary = dir_threshold(image, sobel_kernel=15, thresh=(0.7, 1.3))
+#mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
+#combined = np.zeros_like(dir_binary)
+#combined[((mag_binary == 1) & (dir_binary == 1))] = 1
 # Plot the result
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
 ax1.imshow(image)
 ax1.set_title('Original Image', fontsize=50)
-ax2.imshow(combined, cmap='gray')
+#ax2.imshow(combined, cmap='gray')
+ax2.imshow(combined)
 ax2.set_title('Thresholded Gradients', fontsize=50)
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 plt.show()
